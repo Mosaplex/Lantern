b22dc68b683c8c7129e66ad69433b06c945a6134
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2018-04-04 20:08:59 +0200

    Update for 18w14a

--- src/main/java/org/lanternpowered/server/network/protocol/Protocol.java
+++ src/main/java/org/lanternpowered/server/network/protocol/Protocol.java
@@ -29,7 +29,7 @@ import org.lanternpowered.server.network.message.MessageRegistry;
 
 public interface Protocol {
 
-    int CURRENT_VERSION = 368;
+    int CURRENT_VERSION = 369;
 
     /**
      * Gets the inbound {@link MessageRegistry}.
