674b4f25670e34b3c94dbf5b2ac3e8b707918e96
Seppe Volkaerts <seppevolkaerts@hotmail.com>
2018-04-30 12:25:55 +0200

    Update for 18w16a. Property based lang files can now also be json.

--- src/main/java/org/lanternpowered/server/util/JsonResourceBundle.java
+++ src/main/java/org/lanternpowered/server/util/JsonResourceBundle.java
@@ -23,25 +23,44 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.lanternpowered.server.network.protocol;
+package org.lanternpowered.server.util;
 
-import org.lanternpowered.server.network.message.MessageRegistry;
+import static java.util.Objects.requireNonNull;
 
-public interface Protocol {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
-    int CURRENT_VERSION = 372;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
 
-    /**
-     * Gets the inbound {@link MessageRegistry}.
-     * 
-     * @return The registry
-     */
-    MessageRegistry inbound();
+public final class JsonResourceBundle extends ResourceBundle {
 
-    /**
-     * Gets the outbound {@link MessageRegistry}.
-     * 
-     * @return The registry
-     */
-    MessageRegistry outbound();
+    private static final Gson GSON = new Gson();
+
+    public static JsonResourceBundle loadFrom(InputStream is) {
+        return new JsonResourceBundle(GSON.fromJson(new InputStreamReader(is), JsonObject.class));
+    }
+
+    private final Map<String, String> lookup;
+
+    public JsonResourceBundle(JsonObject jsonObject) {
+        this.lookup = new HashMap<>();
+        jsonObject.entrySet().forEach(e -> this.lookup.put(e.getKey(), e.getValue().getAsString()));
+    }
+
+    @Override
+    protected Object handleGetObject(String key) {
+        requireNonNull(key);
+        return this.lookup.get(key);
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return Collections.enumeration(this.lookup.keySet());
+    }
 }
